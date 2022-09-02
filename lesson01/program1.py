34727fd1a0e0234772c423de8bf379adeaf3a55c
dtnfktu <vtlg@mail.ru>
2022-09-02 13:37:54 +0300

    Updated tasks 1,4 and README.md

--- lesson01/program1.py
+++ lesson01/program1.py
@@ -1,8 +1,17 @@
-week_day = int(input('Введите день недели (число от 1 до 7) : '))
+s = ''                                                      # проверка корректности
+while (not s.isdigit()):                                    # вводимого значения -
+    s = input('Введите день недели (число от 1 до 7) : ')   # принимается только целое неотрицательное число
 
-if week_day <= 0 or week_day > 7:
-    print('Ошибка! Введено некорректное число')
-elif week_day == 6 or week_day == 7:
+week_day = int(s)
+
+if week_day > 7:
+    week_day %= 7
+
+if week_day == 0:
+    week_day = 7
+
+print('Выходной день [', week_day, '] - ', end='')
+if week_day in (6, 7):
     print('да')
 else:
     print('нет')
--- lesson01/program4.py
+++ lesson01/program4.py
@@ -1,4 +1,7 @@
-quarter_number = int(input('Введите номер четверти (1..4) : '))
+s = ''
+while not s.isdigit():
+    s = input('Введите номер четверти (1..4) : ')
+quarter_number = int(s)
 
 if not quarter_number in range(1, 5):
     print('Ошибка! Допустимые значения : 1,2,3,4')
@@ -6,11 +9,11 @@ elif quarter_number == 1:
     print('x = (0..Infinity)')
     print('y = (0..Infinity)')
 elif quarter_number == 2:
-    print('x = (0..-Infinity)')
+    print('x = (-Infinity..0)')
     print('y = (0..Infinity)')
 elif quarter_number == 3:
-    print('x = (0..-Infinity)')
-    print('y = (0..-Infinity)')
+    print('x = (-Infinity..0)')
+    print('y = (-Infinity..0)')
 else:
     print('x = (0..Infinity)')
-    print('y = (0..-Infinity)')
+    print('y = (-Infinity..0)')
