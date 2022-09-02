9e79aab9dc49685fde12a85af87d80433f2c5003
dtnfktu <vtlg@mail.ru>
2022-09-02 09:10:38 +0300

    Task 1 complete

--- lesson01/program1.py
+++ lesson01/program1.py
@@ -1,6 +1,8 @@
-week_day = int(input('Введите день недели (число) : '))
+week_day = int(input('Введите день недели (число от 1 до 7) : '))
 
-if week_day == 6 or week_day == 7:
+if week_day <= 0 or week_day > 7:
+    print('Ошибка! Введено некорректное число')
+elif week_day == 6 or week_day == 7:
     print('да')
 else:
     print('нет')
