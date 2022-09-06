bc79642cba9ba678441b1e8e274e51f1ed655ccd
Виталий Гирик <102477632+dtnfktu@users.noreply.github.com>
2022-09-06 14:11:03 +0300

    Task 1 added

--- draft.py
+++ draft.py
@@ -1,1 +1,23 @@
+# Подсчёт суммы цифр вещественного числа
 
+def IsFloatNumber(a):
+    try:
+        float(a)
+        return True
+    except:
+        return False
+
+str = input('Enter the float number: ').replace(',','.')
+while not IsFloatNumber(str):
+    str = input('Enter the float number: ').replace(',','.')
+
+str = str.replace('-','').replace('.','')
+print(str)
+
+summa = 0
+for s in str:
+    summa += int(s)
+    
+print('Summa = ', summa)
+
+# Подсчёт суммы цифр вещественного числа
