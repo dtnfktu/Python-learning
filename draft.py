01200963b136b3d9afab3fcd31018ee4eee61a92
Виталий Гирик <102477632+dtnfktu@users.noreply.github.com>
2022-09-06 14:31:39 +0300

    Task 2 complete

--- draft.py
+++ draft.py
@@ -20,4 +20,23 @@ for s in str:
     
 print('Summa = ', summa)
 
-# Подсчёт суммы цифр вещественного числа
+# Подсчёт суммы цифр вещественного числа ####################################
+
+# Список произведений чисел #################################################
+
+str = input('Enter the integer number >0 : ')
+while not str.isdigit():
+    str = input('Enter the integer number >0 : ')
+n = int(str)
+
+list = []
+multiply = 1
+
+for i in range(1, n + 1):
+    multiply *= i
+    list.append(multiply)
+    
+print(list)
+
+# Список произведений чисел #################################################
+#
