db4f6350a6da8f059ddba3a72e97b81d88e46751
dtnfktu <vtlg@mail.ru>
2022-09-07 15:34:52 +0300

    Task 2 lesson 2 complete

---
+++ lesson02/program5.py
@@ -0,0 +1,14 @@
+import random
+
+
+list = [i for i in range(1, 16)]
+print('Sorted list')
+print(list)
+
+list_length = len(list)
+for i in range(list_length):
+    index = random.randint(0, list_length - 1)
+    list[i], list[index] = list[index], list[i]
+
+print('Unsorted list')
+print(list)
