9d4c6e91b2d8cddcb16144036266f1d27f9ec7da
Гирик Виталий Сергеевич <vgirik@msk.rsvo.local>
2022-09-07 13:43:43 +0300

    Task 1 lesson 2 complete

---
+++ lesson02/program1.py
@@ -0,0 +1,32 @@
+# Проверяем - введено число, а не что-то иное
+def IsFloatNumber(a):
+    try:
+        float(a)
+        return True
+    except:
+        return False
+
+# Пока не будет введено число цикл повтояертся
+str = input('Enter the number: ').replace(',','.')          # меняем запятую на точку
+while not IsFloatNumber(str):
+    str = input('Enter the number: ').replace(',','.')
+
+# Обрабатываем строку. Убираем минус и точку (если таковые были)
+str = str.replace('-','').replace('.','')
+
+summa = 0
+for s in str:
+    summa += int(s)
+
+# получаем ответ по обработанной строке    
+print('Sum from string =', summa)
+
+a = int(str)
+summa = 0
+
+while (a != 0):
+    summa += a % 10
+    #print(summa)
+    a //= 10
+
+print('Sum from int =', summa)
