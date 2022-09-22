e1ec5936367be9a3690f0cc1ff5b50b32700df07
dtnfktu <vtlg@mail.ru>
2022-09-22 09:18:48 +0300

    Tic-tac-toe complete

--- lesson05/program3.py
+++ lesson05/program3.py
@@ -18,15 +18,28 @@ def set_field(row, col, tictactoe) :
     '''Вносит Х или 0 в заданную ячейку'''
     if  game_field[row][col] == ' ' :
         game_field[row][col] = tictactoe
+        return True
     else :
         print('The cell is occupied, select another one')
+        return False
 
 def select_cell() :
-    row = int(input('Select row : ')) - 1
-    col = int(input('Select col : ')) - 1
+    '''Выбираем координаты ячейки, которую следует заполнить'''
+    row = int(input('Select row (1..3) : ')) - 1
+    col = int(input('Select col (1..3) : ')) - 1
+    return [row, col]
+
+def select_cell_bot() :
+    '''Автоматический выбор пустой ячейки'''
+    row = random.randint(0, 2)
+    col = random.randint(0, 2)
+    while game_field[row][col] != ' ' :
+        row = random.randint(0, 2)
+        col = random.randint(0, 2)
     return [row, col]
 
 def game_over() :
+    '''Проверка на конец игры'''
     # проверка на заполнение игрового поля
     if not ' ' in [x for l in game_field for x in l] :
         return True
@@ -57,23 +70,33 @@ def sign_selection() :
         print('You play by Crosses now')
         return ['X', '0']
 
+# Инициализация игры
+player2 = int(input('Chose player 2 (1 - human, 2 - computer)'))
 game_field = clear_game_field()
 signs = sign_selection()
 current_player = 1 if signs[0] == 'X' else 2
 draw_field()
 
+# Сама игра
 while not game_over() :
+    print('It''s a move now by ' + signs[current_player - 1])
     if current_player == 1 :
         current_cell = select_cell() 
-        set_field(current_cell[0], current_cell[1], signs[current_player - 1])
-        draw_field()
+        while not set_field(current_cell[0], current_cell[1], signs[current_player - 1]) :
+            current_cell = select_cell() 
         current_player = 2
     else :
-        current_cell = select_cell() 
-        set_field(current_cell[0], current_cell[1], signs[current_player - 1])
-        draw_field()
+        if player2 == 1 :
+            current_cell = select_cell() 
+            while not set_field(current_cell[0], current_cell[1], signs[current_player - 1]) :
+                current_cell = select_cell() 
+        else :
+            current_cell = select_cell_bot()
+            set_field(current_cell[0], current_cell[1], signs[current_player - 1])
         current_player = 1
+    draw_field()
 
+print('Game over')
 if ' ' in [x for l in game_field for x in l] :
     print('Player ' + str(1 if current_player == 2 else 1) + ' win')
 else :
