ba242046085eaf4c81652566c2ea3630981c319d
dtnfktu <vtlg@mail.ru>
2022-09-25 15:28:59 +0300

    Replace enumerate on zip in task 1

--- lesson06/program1.py
+++ lesson06/program1.py
@@ -7,7 +7,7 @@ lst = [random.randint(1,100) for i in range(10)]
 print(f'Исходный список:\n{lst}')
 
 # Нумеруем элементы и отсеиваем с чётными индексами
-lt = list(filter(lambda x: x[0] % 2 != 0, list(enumerate(lst,start=0))))
+lt = list(filter(lambda x: x[0] % 2 != 0, list(zip([i for i in range(0,len(lst))],lst))))
 
 # Суммируем элементы и показываем
 print(f'Сумма элементов на нечётных позициях = {sum(list(x[1] for x in lt))}')
